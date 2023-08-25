3de2b0596b354ba6b7124930d93e2ca6bfa8ec69
BobbyChiu <bychiu@utexas.edu>
2023-08-25 15:49:22 -0600

    Interface with cmd line & connect cmd line to main window

--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void runCommand();
+
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
