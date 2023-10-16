b9ab0c645e44a10d2c6ee1ea7b04f161496f4a1d
BobbyChiu <bychiu@utexas.edu>
2023-10-16 16:10:35 -0500

    Add server code

--- src/gui/v2-gui-interface/main.cpp
+++ src/gui/v2-gui-interface/main.cpp
@@ -1,11 +1,31 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+void runServer() {
+    QTcpServer server;
+    if (!server.listen(QHostAddress::LocalHost, 12369)) {
+        qDebug() << "Server could not start. Error: " << server.errorString();
+        return;
+    }
+    qDebug() << "Server is listening on port 12345.";
+
+    QObject::connect(&server, &QTcpServer::newConnection, [&]() {
+        QTcpSocket* client = server.nextPendingConnection();
+        QObject::connect(client, &QTcpSocket::readyRead, [&]() {
+            QByteArray data = client->readAll();
+            qDebug() << "Received data from client: " << data;
+        });
+    });
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    runServer();
     return a.exec();
 }
