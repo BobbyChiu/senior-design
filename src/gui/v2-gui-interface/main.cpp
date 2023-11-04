53f4c7ccb27f0375d364b64d97d9e140531085bf
BobbyChiu <bychiu@utexas.edu>
2023-11-04 08:06:16 -0500

    Remove commented server code

--- src/gui/v2-gui-interface/main.cpp
+++ src/gui/v2-gui-interface/main.cpp
@@ -1,50 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-//#include <QTcpServer>
-//#include <QTcpSocket>
-
-//void runServer() {
-//    QTcpServer server;
-//    if (!server.listen(QHostAddress::LocalHost, 12369)) {
-//        qDebug() << "Server could not start. Error: " << server.errorString();
-//        return;
-//    }
-//    qDebug() << "Server is listening on port 12369.";
-
-//    server.waitForNewConnection(-1);
-//    QTcpSocket *client = server.nextPendingConnection();
-//    qDebug() << "Connected.";
-
-//    while (true)
-//    {
-//        client->waitForReadyRead();
-//        char data[16];
-//        auto len = client->read(data, 16);
-//        data[len] = '\0';
-//        qDebug() << "Received data from client: " << data << len;
-//        if (strcmp(data, "done") == 0)
-//        {
-//            break;
-//        }
-//    }
-
-//    /*
-//    QObject::connect(&server, &QTcpServer::newConnection, [&]() {
-//        QTcpSocket* client = server.nextPendingConnection();
-//        QObject::connect(client, &QTcpSocket::readyRead, [&]() {
-//            QByteArray data = client->readAll();
-//            qDebug() << "Received data from client: " << data;
-//        });
-//    });
-//    */
-//}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    //runServer();
     return a.exec();
 }
